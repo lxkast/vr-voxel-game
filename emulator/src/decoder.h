82548903618dd346476286435ed2950ee67c7e53
Lukas <lukastrakim@gmail.com>
2025-05-24 15:56:01 +0100

    [lt724] feat(emulator): added cmd line file reading to emulator

---
+++ emulator/src/decoder.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <stdint.h>
+
+/*
+    Creating bitfields for all the different types of instruction
+*/
+
+typedef struct {
+    uint8_t sf : 1;
+    uint8_t opc : 2;
+    uint8_t : 3;
+    uint8_t opi : 3;
+    uint32_t operand : 18;
+    uint8_t rd : 5;
+} DPImmInstruction_t;
+
+typedef struct {
+    uint8_t sf : 1;
+    uint8_t opc : 2;
+    uint8_t m : 1;
+    uint8_t : 3;
+    uint8_t opr : 4;
+    uint8_t rm : 5;
+    uint8_t operand : 6;
+    uint8_t rn : 5;
+    uint8_t rd : 5;
+} DPRegInstruction_t;
+
+typedef struct {
+    uint8_t : 1;
+    uint8_t sf : 1;
+    uint8_t : 7;
+    uint8_t l : 1;
+    uint16_t offset : 12;
+    uint8_t xn : 5;
+    uint8_t rt : 5;
+} SDTInstruction_t;
+
+typedef struct {
+    uint8_t : 1;
+    uint8_t sf : 1;
+    uint8_t : 6;
+    uint32_t simm19 : 19;
+    uint8_t rt : 5;
+} loadLitInstruction_t;
+
+typedef struct {
+    uint8_t type : 2;
+    uint8_t : 4;
+    uint32_t operand : 26;
+} branchInstruction_t;
+
+typedef union {
+    uint32_t raw;
+    DPImmInstruction_t dpimm;
+    DPRegInstruction_t dpreg;
+    SDTInstruction_t sdt;
+    loadLitInstruction_t load;
+    branchInstruction_t branch;
+} instruction_u;
+
+
+
+void decodeAndExecute(const uint32_t rawInstruction);
--- emulator/src/emulate.c
+++ emulator/src/emulate.c
@@ -1,9 +1,24 @@
 #include <stdlib.h>
 #include <logging.h>
+#include <futil.h>
+#include "state.h"
+#include "decoder.h"
 
 int main(int argc, char **argv) {
     log_init(stdout);
-    LOG_INFO("Emulator started.");
+    if (argc < 2) {
+        LOG_INFO("Usage: emulate <file_in> <file_out>");
+    }
+    char* fileIn = argv[1];
+    futil_file_t *fp = futil_open(fileIn, FUTIL_FILE_READ);
+    if (!fp) {
+        LOG_FATAL("Failed to open file %s", fileIn);
+    }
+    int numInstructions = futil_fileSize(fp);
+    uint32_t *programInstructions = (uint32_t *)malloc(numInstructions * sizeof(uint32_t));
+    futil_readBinary(fp, programInstructions, numInstructions);
 
+
+    futil_close(fp);
     return EXIT_SUCCESS;
 }
