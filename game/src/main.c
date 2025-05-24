cb94a888058a4cfc5658e48e8401e5fbc966db8a
Lukas <lukastrakim@gmail.com>
2025-05-24 22:20:19 +0100

    [lt724][taz24] experiment(extension): remove support for OpenGL ES

--- game/src/main.c
+++ game/src/main.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
-#ifdef USE_GL
 #include <glad/gl.h>
-#elifdef USE_GLES
-#include <glad/gles2.h>
-#endif
 #include <GLFW/glfw3.h>
 #include <logging.h>
-
-#ifdef USE_GL
-#define MINOR_VERSION 3
-#else
-#define MINOR_VERSION 1
-#endif
-
 
 int main(void) {
     log_init(stdout);
 
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, MINOR_VERSION);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow *window = glfwCreateWindow(640, 480, "Hello, Window!", NULL, NULL);
@@ -36,7 +25,7 @@ int main(void) {
         return -1;
     }
 
-    {
+        {
         int width, height;
         glfwGetFramebufferSize(window, &width, &height);
         glViewport(0, 0, width, height);
