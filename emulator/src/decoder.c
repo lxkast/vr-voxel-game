cb7c5eddec6ba9552c5fa65e6b05f7859e4f236c
Lukas <lukastrakim@gmail.com>
2025-05-24 15:56:01 +0100

    [lt724] feat(emulator): added cmd line file reading to emulator

--- emulator/src/decoder.c
+++ emulator/src/decoder.c
@@ -1,4 +1,4 @@
-#include <stdint.h>
+#include "decoder.h"
 #include "logging.h"
 /*
     Defining masks, expected values to be used in later functions
@@ -19,63 +19,7 @@
 #define BRANCH_MASK 0x1c000000        // 2^28 + 2^27 + 2^26
 #define BRANCH_VALUE 0x14000000       // 2^28 + 2^26
 
-/*
-    Creating structs for all the different types of instruction
-*/
 
-typedef struct {
-    uint8_t sf : 1;
-    uint8_t opc : 2;
-    uint8_t : 3;
-    uint8_t opi : 3;
-    uint32_t operand : 18;
-    uint8_t rd : 5;
-} DPImmInstruction_t;
-
-typedef struct {
-    uint8_t sf : 1;
-    uint8_t opc : 2;
-    uint8_t m : 1;
-    uint8_t : 3;
-    uint8_t opr : 4;
-    uint8_t rm : 5;
-    uint8_t operand : 6;
-    uint8_t rn : 5;
-    uint8_t rd : 5;
-} DPRegInstruction_t;
-
-typedef struct {
-    uint8_t : 1;
-    uint8_t sf : 1;
-    uint8_t : 7;
-    uint8_t l : 1;
-    uint16_t offset : 12;
-    uint8_t xn : 5;
-    uint8_t rt : 5;
-} SDTInstruction_t;
-
-typedef struct {
-    uint8_t : 1;
-    uint8_t sf : 1;
-    uint8_t : 6;
-    uint32_t simm19 : 19;
-    uint8_t rt : 5;
-} loadLitInstruction_t;
-
-typedef struct {
-    uint8_t type : 2;
-    uint8_t : 4;
-    uint32_t operand : 26;
-} branchInstruction_t;
-
-typedef union {
-    uint32_t raw;
-    DPImmInstruction_t dpimm;
-    DPRegInstruction_t dpreg;
-    SDTInstruction_t sdt;
-    loadLitInstruction_t load;
-    branchInstruction_t branch;
-} instruction_u;
 
 /*
     Takes an instruction and determines what type it is, then uses a subsequent
