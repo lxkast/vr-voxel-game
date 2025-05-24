e6426783aaa37592a0d1be0cbdfe5600672194a7
Thomas Zachary <thomaszachary06@proton.me>
2025-05-24 19:53:04 +0100

    [taz24][lt724] feat(extension): Implement hello window

--- game/src/main.c
+++ game/src/main.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
+#ifdef USE_GL
+#include <glad/gl.h>
+#elifdef USE_GLES
+#include <glad/gles2.h>
+#endif
+#include <GLFW/glfw3.h>
 
 int main(void) {
-    printf("Hello, extension!\n");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Hello, Window!", NULL, NULL);
+    if (window == NULL) {
+        // TODO: logs
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        // TODO: logs
+        glfwTerminate();
+        return -1;
+    }
+
+    {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    return 0;
 }
