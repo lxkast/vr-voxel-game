facc0cb55d0334f1aa07ce3db0652e81051c1c93
Lukas <lukastrakim@gmail.com>
2025-05-26 20:57:15 +0100

    [lt724] feature(extension): add cube vertices and indices

---
+++ game/src/vertices.c
@@ -0,0 +1,35 @@
+#include "vertices.h"
+
+const float cubeVertices[] = {
+    // Front face
+    -0.5f, -0.5f,  0.5f,  // 0
+     0.5f, -0.5f,  0.5f,  // 1
+     0.5f,  0.5f,  0.5f,  // 2
+    -0.5f,  0.5f,  0.5f,  // 3
+    // Back face
+    -0.5f, -0.5f, -0.5f,  // 4
+     0.5f, -0.5f, -0.5f,  // 5
+     0.5f,  0.5f, -0.5f,  // 6
+    -0.5f,  0.5f, -0.5f   // 7
+};
+
+const float cubeIndices[] = {
+    // Front face
+    0, 1, 2,
+    2, 3, 0,
+    // Right face
+    1, 5, 6,
+    6, 2, 1,
+    // Back face
+    5, 4, 7,
+    7, 6, 5,
+    // Left face
+    4, 0, 3,
+    3, 7, 4,
+    // Top face
+    3, 2, 6,
+    6, 7, 3,
+    // Bottom face
+    4, 5, 1,
+    1, 0, 4
+};
---
+++ game/src/vertices.h
@@ -0,0 +1,4 @@
+#pragma once
+
+extern const float cubeVertices[];
+extern const float cubeIndices[];
