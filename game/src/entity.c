b2fe13314530ebbbc6c30602e32369eb70da9145
Sean Robinson <sean.robinson24@imperial.ac.uk>
2025-05-29 15:36:19 +0100

    [sr2024] feat(entity.c): Added collision handling (untested)

--- game/src/entity.c
+++ game/src/entity.c
@@ -23,31 +23,62 @@ aabb_t makeAABB(const entity_t *entity) {
     return box;
 }
 
-void moveEntity(const entity_t *entity, const vec3 moveVec) {
+// this is TEMPORARY, will be updated when merging onto main branch
+typedef enum {
+    AIR,
+    DIRT,
+    STONE,
+} block_type_e;
+typedef struct {
+    vec3 position;
+    block_type_e type;
+    aabb_t aabb;
+} block_t;
+
+
+void moveEntity(entity_t *entity, const vec3 moveVec) {
     const aabb_t aabb = makeAABB(entity);
 
     // Not yet implemented - will get all adjacent blocks once that is implemented
-    const aabb_t blocks[];
+    const block_t blocks[];
     // assuming there are 12
 
     for (int i = 0; i < 12; i++) {
-        if (intersectsX(aabb, blocks[i]) && intersectsZ(aabb, blocks[i])) {
-            if (aabb.min[1] + moveVec[1] < blocks[i].max[1] && aabb.max[1] + moveVec[1] >= blocks[i].min[1]) {
-                // TODO: handle Y-axis collision
+        if (blocks[i].type == AIR) { continue; }
+        if (intersectsX(aabb, blocks[i].aabb) && intersectsZ(aabb, blocks[i].aabb)) {
+            if (aabb.min[1] + moveVec[1] < blocks[i].aabb.max[1] && aabb.max[1] + moveVec[1] >= blocks[i].aabb.min[1]) {
+                if (moveVec[1] < 0) {
+                    entity->position[1] = blocks[i].aabb.max[1];
+                } else {
+                    entity->position[1] = blocks[i].aabb.min[1] - entity->size[1];
+                }
+                entity->velocity[1] = 0;
             }
         }
     }
     for (int i = 0; i < 12; i++) {
-        if (intersectsY(aabb, blocks[i]) && intersectsZ(aabb, blocks[i])) {
-            if (aabb.min[0] + moveVec[0] < blocks[i].max[0] && aabb.max[0] + moveVec[0] >= blocks[i].min[0]) {
-                // TODO: handle X-axis collision
+        if (blocks[i].type == AIR) { continue; }
+        if (intersectsY(aabb, blocks[i].aabb) && intersectsZ(aabb, blocks[i].aabb)) {
+            if (aabb.min[0] + moveVec[0] < blocks[i].aabb.max[0] && aabb.max[0] + moveVec[0] >= blocks[i].aabb.min[0]) {
+                if (moveVec[0] < 0) {
+                    entity->position[0] = blocks[i].aabb.max[0];
+                } else {
+                    entity->position[0] = blocks[i].aabb.min[0] - entity->size[0];
+                }
+                entity->velocity[0] = 0;
             }
         }
     }
     for (int i = 0; i < 12; i++) {
-        if (intersectsX(aabb, blocks[i]) && intersectsY(aabb, blocks[i])) {
-            if (aabb.min[2] + moveVec[2] < blocks[i].max[2] && aabb.max[2] + moveVec[2] >= blocks[i].min[2]) {
-                // TODO: handle Z-axis collision
+        if (blocks[i].type == AIR) { continue; }
+        if (intersectsX(aabb, blocks[i].aabb) && intersectsY(aabb, blocks[i].aabb)) {
+            if (aabb.min[2] + moveVec[2] < blocks[i].aabb.max[2] && aabb.max[2] + moveVec[2] >= blocks[i].aabb.min[2]) {
+                if (moveVec[2] < 0) {
+                    entity->position[2] = blocks[i].aabb.max[0];
+                } else {
+                    entity->position[2] = blocks[i].aabb.min[2] - entity->size[2];
+                }
+                entity->velocity[2] = 0;
             }
         }
     }
