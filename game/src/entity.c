b1e266921388591ab940bbd6c75a6a33f01832c4
Sean Robinson <sean.robinson24@imperial.ac.uk>
2025-05-30 10:28:32 +0100

    [sr2024] style(entity.c): Added comments to the code describing functions

--- game/src/entity.c
+++ game/src/entity.c
@@ -16,6 +16,11 @@ bool intersects(const aabb_t box1, const aabb_t box2) {
     return intersectsX(box1,box2) && intersectsY(box1,box2) && intersectsZ(box1,box2);
 }
 
+/**
+ * @brief Calculates and returns a bounding box for an entity
+ * @param entity The entity whose bounding box we want
+ * @return The entity's bounding box
+ */
 aabb_t makeAABB(const entity_t *entity) {
     aabb_t box;
     glm_vec3_sub(entity->position, entity->size, box.min);
@@ -38,15 +43,24 @@ typedef struct {
 // Not yet implemented - will get all adjacent blocks once that is implemented
 extern block_t* getAdjacentBlocks(vec3 position, vec3 size, int *numBlocks);
 
-void handleAxisCollision(entity_t *entity, const aabb_t aabb, const block_t *blocks, vec3 deltaP, const int index, const int axisNum) {
-    if (aabb.min[axisNum] + deltaP[axisNum] < blocks[index].aabb.max[axisNum] && aabb.max[axisNum] + deltaP[axisNum] >= blocks[index].aabb.min[axisNum]) {
+/**
+ * @brief Handles a collision of an entity and a block along a specific axis. Tries to
+ *        resolve collisions by updating deltaP so the entity never moves inside a block.
+ * @param entity The entity that is colliding
+ * @param aabb The entity's bounding box
+ * @param block The block we are checking for collisions with
+ * @param deltaP The amount we were originally planning on moving the entity by
+ * @param axisNum The axis we are resolving on
+ */
+void handleAxisCollision(entity_t *entity, const aabb_t aabb, const block_t block, vec3 deltaP, const int axisNum) {
+    if (aabb.min[axisNum] + deltaP[axisNum] < block.aabb.max[axisNum] && aabb.max[axisNum] + deltaP[axisNum] >= block.aabb.min[axisNum]) {
         if (deltaP[axisNum] < 0) {
-            deltaP[axisNum] = blocks[index].aabb.max[axisNum] - entity->position[axisNum];
+            deltaP[axisNum] = block.aabb.max[axisNum] - entity->position[axisNum];
             if (axisNum == 1) {
                 entity->grounded = true;
             }
         } else {
-            deltaP[axisNum] = blocks[index].aabb.min[axisNum] - entity->size[axisNum] - entity->position[axisNum];
+            deltaP[axisNum] = block.aabb.min[axisNum] - entity->size[axisNum] - entity->position[axisNum];
         }
         entity->velocity[axisNum] = 0;
     }
@@ -68,31 +82,39 @@ void moveEntity(entity_t *entity, vec3 deltaP) {
     for (int i = 0; i < numBlocks; i++) {
         if (blocks[i].type == AIR) { continue; }
         if (intersectsX(aabb, blocks[i].aabb) && intersectsZ(aabb, blocks[i].aabb)) {
-            handleAxisCollision(entity, aabb, blocks, deltaP, i, 1);
+            handleAxisCollision(entity, aabb, blocks[i], deltaP, 1);
         }
     }
     // resolves collisions in X-axis
     for (int i = 0; i < numBlocks; i++) {
         if (blocks[i].type == AIR) { continue; }
         if (intersectsY(aabb, blocks[i].aabb) && intersectsZ(aabb, blocks[i].aabb)) {
-            handleAxisCollision(entity, aabb, blocks, deltaP, i, 0);
+            handleAxisCollision(entity, aabb, blocks[i], deltaP, 0);
         }
     }
     // resolves collisions in Z-axis
     for (int i = 0; i < numBlocks; i++) {
         if (blocks[i].type == AIR) { continue; }
         if (intersectsX(aabb, blocks[i].aabb) && intersectsY(aabb, blocks[i].aabb)) {
-            handleAxisCollision(entity, aabb, blocks, deltaP, i, 2);
+            handleAxisCollision(entity, aabb, blocks[i], deltaP, 2);
         }
     }
 
     glm_vec3_add(entity->position, deltaP, entity->position);
 }
 
+/**
+ * @brief Clamps a value between a minimum and maximum value
+ */
 static float clamp(const float value, const float min, const float max) {
     return (value < min) ? min : (value > max) ? max : value;
 }
 
+/**
+ * @brief Updates an entity's velocity
+ * @param entity The entity whose velocity we are updating
+ * @param deltaV The amount we want to update that velocity by
+ */
 void updateVelocity(entity_t *entity, vec3 deltaV) {
     glm_vec3_add(entity->velocity, deltaV, entity->velocity);
 
