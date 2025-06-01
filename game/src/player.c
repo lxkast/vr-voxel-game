aa01051d0af0e2dfd5b472c758b40564349dbaf2
Thomas Zachary <thomaszachary06@proton.me>
2025-06-01 21:19:43 +0100

    [taz24] style(extension(player)): Format player.c

--- game/src/player.c
+++ game/src/player.c
@@ -2,15 +2,15 @@
 
 void player_init(player_t *p) {
     p->cameraPitch = 0.f;
-    *p = (player_t) {
+    *p = (player_t){
         .entity = {
-            .position = {0.f, 15.f, 0.f},
-            .velocity = {0.f, 0.f, 0.f},
-            .size = {0.6f, 1.8f, 0.6f},
-            .acceleration = {0.f, 0.f, 0.f},
-            .grounded = false,
-            .yaw = 0,
-        },
+                   .position = {0.f, 15.f, 0.f},
+                   .velocity = {0.f, 0.f, 0.f},
+                   .size = {0.6f, 1.8f, 0.6f},
+                   .acceleration = {0.f, 0.f, 0.f},
+                   .grounded = false,
+                   .yaw = 0,
+                   },
         .cameraPitch = 0.f,
         .cameraOffset = {0.3f, 1.6f, 0.3f}
     };
@@ -22,18 +22,18 @@ void player_attachCamera(player_t *p, camera_t *camera) {
     camera_setPos(camera, camPos);
 
     vec3 BlockPosition;
-    vec3 sub1 = {0.f,1.f,0.f};
+    vec3 sub1 = {0.f, 1.f, 0.f};
     glm_vec3_sub(p->entity.position, sub1, BlockPosition);
-    glm_vec3_floor(BlockPosition,BlockPosition);
+    glm_vec3_floor(BlockPosition, BlockPosition);
 
     const float qx = camera->ori[0];
     const float qy = camera->ori[1];
     const float qz = camera->ori[2];
     const float qw = camera->ori[3];
 
-    const float sinyCosp = 2.0f * ( qw*qy + qx*qz );
-    const float cosyCosp = 1.0f - 2.0f * ( qy*qy + qz*qz );
-    const float yaw = atan2f(sinyCosp , cosyCosp);
+    const float sinyCosp = 2.0f * (qw * qy + qx * qz);
+    const float cosyCosp = 1.0f - 2.0f * (qy * qy + qz * qz);
+    const float yaw = atan2f(sinyCosp, cosyCosp);
 
     p->entity.yaw = yaw;
 }
