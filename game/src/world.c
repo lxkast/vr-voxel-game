f2c6b7571b1437ef45a468f56016c912565bee38
Sean Robinson <sean.robinson24@imperial.ac.uk>
2025-06-05 18:37:33 +0100

    [sr2024] fix(extension): Fixed bugs (chunks no longer derender when partially in view

--- game/src/world.c
+++ game/src/world.c
@@ -135,22 +135,17 @@ vec3 chunkBounds = {15.f, 15.f, 15.f};
 
 // Note - we assume lookVector is normalised
 static bool isChunkInFrontOfCamera(vec3 camPos, vec3 lookVector, const chunk_t *chunk) {
-    vec3 chunkCoordsAdjusted;
-    chunkCoordsAdjusted[0] = (float)(chunk->cx << 4);
-    chunkCoordsAdjusted[1] = (float)(chunk->cy << 4);
-    chunkCoordsAdjusted[2] = (float)(chunk->cz << 4);
-
     vec3 chunkCenter;
-    chunkCenter[0] = chunkCoordsAdjusted[0] + 8.f;
-    chunkCenter[1] = chunkCoordsAdjusted[1] + 8.f;
-    chunkCenter[2] = chunkCoordsAdjusted[2] + 8.f;
+    chunkCenter[0] = (float)(chunk->cx << 4) + 8.f;
+    chunkCenter[1] = (float)(chunk->cy << 4) + 8.f;
+    chunkCenter[2] = (float)(chunk->cz << 4) + 8.f;
 
     vec3 toChunk;
     glm_vec3_sub(chunkCenter, camPos, toChunk);
 
     const float dot = glm_vec3_dot(toChunk, lookVector);
 
-    return dot < 0;
+    return dot < 16.f;
 }
 
 void world_draw(const world_t *w, const int modelLocation, vec3 camPos, vec3 lookVector) {
@@ -159,9 +154,7 @@ void world_draw(const world_t *w, const int modelLocation, vec3 camPos, vec3 loo
         for (int i = 0; i < C_T * C_T * C_T; i++) {
             if (!cluster->cells[i].chunk) {continue;}
             const bool renderingChunk = isChunkInFrontOfCamera(camPos, lookVector, cluster->cells[i].chunk);
-            // if (!renderingChunk && i < 5) {
-            //     LOG_DEBUG("Not rendering chunk");
-            // }
+
             if (cluster->cells[i].chunk && renderingChunk) {
                 chunk_draw(cluster->cells[i].chunk, modelLocation);
             }
