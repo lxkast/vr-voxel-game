9f471357c967f5e1f71c39252f813c3eb1367e7e
Sean Robinson <sean.robinson24@imperial.ac.uk>
2025-06-10 15:57:24 +0100

    [sr2024] feat(extension): Uncommented FPS code

--- game/src/main.c
+++ game/src/main.c
@@ -339,11 +339,11 @@ int main(void) {
 
         glUseProgram(0);
 
-        // fpsDisplayAcc += analytics.dt;
-        // if (fpsDisplayAcc > 1.0) {
-        //     LOG_INFO("%.0lf\n", analytics.fps);
-        //     fpsDisplayAcc = 0.0;
-        // }
+        fpsDisplayAcc += analytics.dt;
+        if (fpsDisplayAcc > 1.0) {
+            LOG_INFO("%.0lf\n", analytics.fps);
+            fpsDisplayAcc = 0.0;
+        }
 
         glfwPollEvents();
         glfwSwapBuffers(window);
